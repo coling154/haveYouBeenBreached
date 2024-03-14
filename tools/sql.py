bfac05447c35bff8f4a2d136f9c748bfe5e7c36d
Merpster456 <ryan.quirk6@protonmail.ch>
2024-03-13 20:09:24 -0400

    Removed query time checks. Added comments for all remaining uncommented code.

--- tools/sql.py
+++ tools/sql.py
@@ -1,6 +1,7 @@
 """
 sql.py
 Created by Colin Gasiewicz on 03/06/2024
+Authors: Colin Gasiewicz, Ryan Quirk
 This script compliments input.py
 it contains sql functions to interface with the database
 such as inserting and searching the data
@@ -45,53 +46,36 @@ def insert(usr, pas, con):
         print("User already exists: " + usr)
     except Error as e:
         print("Error occurred:", e)
-def check(usr, pas, con):
-    """
-    Check if the user exists
-    :param usr: user hash string
-    :type usr: str
-    :param pas: password hash string
-    :type pas: str
-    :param con: connection object
-    :return: true if user exists and false otherwise
-    :rtype: int
-    """
-    connection = con
-    cursor = connection.cursor()
-    query = "SELECT username FROM users WHERE username = %s LIMIT 1"
-    cursor.execute(query, (usr,))
-    # if email is in database
-    if cursor.fetchone():
-        query = "SELECT password FROM users WHERE password = %s AND username = %s LIMIT 1"
-        cursor.execute(query, (pas, usr))
-        # email is in database but password is not
-        status = 2
-        if cursor.fetchone():
-            # email and password are in database
-            status = 0
-    else:
-        # email is not in database
-        status = 1
-    return status
 
-"""
-Adding the skeleton to functions checkEmail and checkPassword
-that will be accessed by application.
-"""
+
 
 def checkEmail(email, con):
+    """
+    Query the database with the passed connection (param: con).
+    The query is created with the email hash that is passed (param: email).
+    Returns a boolean according to the result of the query, i.e. the email
+    found in the database returns True.
+    """
     cursor = con.cursor()
-    query = f'SELECT username FROM users WHERE username = "{email}" LIMIT 1'
 
+    # Limit the query to 1 result to save time.
+    query = f'SELECT username FROM users WHERE username = "{email}" LIMIT 1'
     cursor.execute(query)    
 
     if cursor.fetchone(): return True
     else: return False
 
 def checkPassword(pwd, con):
+    """
+    Query the database with the passed connection (param: con).
+    The query is created with the password hash that is passed (param: pwd).
+    Returns a boolean according to the result of the query, i.e. the password
+    NOT found in the database returns False.
+    """
     cursor = con.cursor()
-    query = f'SELECT password FROM users WHERE password = "{pwd}" LIMIT 1'
 
+    # Limit the query to 1 result to save time.
+    query = f'SELECT password FROM users WHERE password = "{pwd}" LIMIT 1'
     cursor.execute(query)    
 
     if cursor.fetchone(): return True
